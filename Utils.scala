269d458484064322b2d0d92bb00546f3fe43a1b1
PGoncalo17 <pedro.gcbf@gmail.com>
2026-04-13 15:28:30 +0100

    Utils fix

--- Utils.scala
+++ Utils.scala
@@ -14,23 +14,25 @@ object Utils {
     
     //build a single line   
     def buildLine(line:Int, col:Int, maxCols:Int, board:Board): String = {
-        if(col >= maxCols) ""  //end of line
-        else{
-            val symbol = board.get((line, col)) match {     //choose symbol for each piece
-                case None => "   "
-                case Some(Stone.White) => " W "
-                case Some(Stone.Black) => " B "
-            }
-            symbol + buildLine(line, col + 1, maxCols, board)  //next colunm
+        (col >= maxCols) match {
+            case true => ""  //end of line
+            case false =>        
+                val symbol = board.get((line, col)) match {     //choose symbol for each piece
+                    case None => "   "
+                    case Some(Stone.White) => " W "
+                    case Some(Stone.Black) => " B "
+                }
+                symbol + buildLine(line, col + 1, maxCols, board)  //next colunm
         }
     }
     //build all matrix
     def buildAllLines(line:Int, maxLines:Int, maxCols:Int, board:Board): String = {
-        if(line >= maxLines) ""     //end of colunm
-        else {
-            val lineSymbols = buildLine(line, 0, maxCols, board)        //build line
-            val lineLable = if(line < 10) " " + line.toString + " " else line.toString + " "    //fix spaces for numbers < 10
-            lineLable + lineSymbols + "\n" + buildAllLines(line + 1, maxLines, maxCols, board)  //next line
+        (line >= maxLines) match {
+            case true => ""     //end of colunm
+            case false =>
+                val lineSymbols = buildLine(line, 0, maxCols, board)        //build line
+                val lineLable = if(line < 10) " " + line.toString + " " else line.toString + " "    //fix spaces for numbers < 10
+                lineLable + lineSymbols + "\n" + buildAllLines(line + 1, maxLines, maxCols, board)  //next line
         }
     }
 
@@ -38,7 +40,10 @@ object Utils {
     def printBoard(board: Board, lines: Int, cols: Int): Unit = {
         val colIndexes = (0 until cols).toList      //Create list of numbers for each colunm
         val headderLetters = (colIndexes foldRight ("")){ (c, acc) =>   //foldRight to put numbers in right place
-            val letter = if (c < 10) s" $c " else s"$c "
+            val letter = (c < 10) match {
+                case true => s" $c " 
+                case false => s"$c "
+            }
             letter + acc
             }
         println("   " + headderLetters)
